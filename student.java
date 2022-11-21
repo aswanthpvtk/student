d05daf7b55038be99e5bf0ee72502f5fc40821dd
Aswanth P <Aswanth@NTP-LAP-734.chn.nestgroup.net>
2022-11-22 01:21:30 +0530

    std

--- student.java
+++ student.java
@@ -3,6 +3,34 @@ import java.util.Scanner;
 
 public class student {
 
+    String name;
+    int adno;
+    int rollno;
+    String cname;
+
+    public student(String name, int adno, int rollno, String cname) {
+        this.name = name;
+        this.adno = adno;
+        this.rollno = rollno;
+        this.cname = cname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAdno() {
+        return adno;
+    }
+
+    public int getRollno() {
+        return rollno;
+    }
+
+    public String getCname() {
+        return cname;
+    }
+
     public static void main(String[] args) {
         int op;
         ArrayList<student> arr = new ArrayList<student>();
@@ -12,6 +40,24 @@ public class student {
             System.out.println("1.Add\n2.View\n3.Search\n4.Delete\n5.Exit");
             Scanner sc = new Scanner(System.in);
             op = sc.nextInt();
+
+            switch(op)
+            {
+                case 1:
+                    System.out.println("Enter the student name");
+                    String name = sc.next();
+                    System.out.println("Enter the ad no");
+                    int adno = sc.nextInt();
+                    System.out.println("enter the rollno");
+                    int rollno = sc.nextInt();
+                    System.out.println("enter the college name");
+                    String cname = sc.next();
+                    student e = new student(name, adno, rollno, cname);
+                    arr.add(e);
+                    break;
+
+
+            }
 
     }
 }
